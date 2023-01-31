107b3cdb859e8ed740a7767f0e4c37e383fca997
Carlos Blanco <acbgalan@gmail.com>
2023-01-31 01:05:00 +0100

    Controlador Pizzas, método para recuperar todas las Pizzas.

---
+++ src/pizza-orders/Controllers/PizzasController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pizza_orders.data;
+using pizza_orders.data.Models;
+
+namespace pizza_orders.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PizzasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Pizza>>> GetPizzas()
+        {
+            var pizzas = await _context.Pizzas.ToListAsync();
+            return Ok(pizzas);
+        }
+
+    }
+}
