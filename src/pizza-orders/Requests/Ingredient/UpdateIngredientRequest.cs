d41033744f79394bdb44daf01c5d21141a2f2b04
Carlos Blanco <acbgalan@gmail.com>
2023-02-05 01:42:02 +0100

    Clases para crear, actualizar y recibicir ingredientes

---
+++ src/pizza-orders/Requests/Ingredient/UpdateIngredientRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pizza_orders.Requests.Ingredient
+{
+    public class UpdateIngredientRequest
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(50, ErrorMessage = "Se esperaba una cadena de texto en el campo {0} con un máximo de {1} caracteres")]
+        public string Name { get; set; }
+    }
+}
