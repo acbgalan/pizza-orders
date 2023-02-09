079eaccb6663256c7b2f118e2f5722f89a232873
Carlos Blanco <acbgalan@gmail.com>
2023-02-09 22:55:23 +0100

    Módelo para Clientes

---
+++ src/pizza-orders.data/Models/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pizza_orders.data.Models
+{
+    [Table("Clients")]
+    public class Client
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(50, ErrorMessage = "Se esperaba una cadena de texto en el campo {0} con un máximo de {1} caracteres")]
+        [DisplayName("Nombre")]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
