c7763ca5de2f786a48712a55edda503c06d8aab5
Carlos Blanco <acbgalan@gmail.com>
2023-02-09 23:36:41 +0100

    Módelo para pedidos. Order

---
+++ src/pizza-orders.data/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizza_orders.data.Models
+{
+    public enum PaymentMethod { Cash, Card, Prepaid }
+
+    [Table("Orders")]
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Fecha")]
+        public DateTime Date { get; set; }
+
+        [DisplayName("Método de pago")]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [Precision(9, 2)]
+        [DisplayName("Precio")]
+        public decimal Prize { get; set; }
+
+        [ForeignKey("Client")]
+        public int ClientId { get; set; }
+
+        public Client Client { get; set; }
+    }
+}
