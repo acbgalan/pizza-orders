969391ff4211f4eb3349093878fe85bc5cae64fb
Carlos Blanco <acbgalan@gmail.com>
2023-02-13 01:17:17 +0100

    Repositorio para pedidos, se añade como servicio.

--- src/pizza-orders/Program.cs
+++ src/pizza-orders/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 builder.Services.AddScoped<IPizzaRepository, PizzaRepository>();
 builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 builder.Services.AddControllers();
