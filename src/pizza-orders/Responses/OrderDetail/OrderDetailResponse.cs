349e314fdcfda2ef93dd196cc2232e8c9bdb4a8e
Carlos Blanco <acbgalan@gmail.com>
2023-02-16 00:02:30 +0100

    Trabajando controlador de pedidos. Controlador, Responses, etc.. recuperamos pedido sin sus lineas.

---
+++ src/pizza-orders/Responses/OrderDetail/OrderDetailResponse.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pizza_orders.data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace pizza_orders.Responses.OrderDetail
+{
+    public class OrderDetailResponse
+    {
+        public int OrderId { get; set; }
+        public int PizzaId { get; set; }
+
+        [DisplayName("Precio unitario")]
+        [Precision(9, 2)]
+        public decimal UnitPrize { get; set; }
+
+        [DisplayName("Cantidad")]
+        public int Quantity { get; set; }
+
+        [DisplayName("Descuento")]
+        [Range(0, 100, ErrorMessage = "Se esperaba un valor entre {1} y {2}")]
+        public byte Discount { get; set; }
+
+        [DisplayName("Importe")]
+        [Precision(9, 2)]
+        public decimal Amount { get; set; }
+    }
+}
